19ffafb1dd0630923a5d2f936fd434c9182a813d
Adrial-Knight <pierreminier64@gmail.com>
2022-12-13 22:33:31 +0100

    [Resultat]
    Ajout de la figure sur l'erreur commise tout au long de la trajectoire du véhicule

--- function/display/display_quadratic_error.m
+++ function/display/display_quadratic_error.m
@@ -1,8 +1,9 @@
 function display_quadratic_error(x1, x2)
-    error = vecnorm(x1 - x2, 2, 2);
+    error = vecnorm(x1 - x2);
     figure("Name", "Erreur quadratique")
     plot(error, Color="#127BCA")
     grid
     axis tight
     xlabel("t");
+    fprintf("Ecart maximal: " + max(error) + "m\n")
 end
